d888d0fa899de5bc1e3429165136fe6308d29a6a
geoffrey <geoffreyherman_19@hotmail.com>
2020-01-23 20:29:01 +0100

    bonjour

---
+++ l2s4/TW2/seance1/iniPHP.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Premier exercice PHP</title>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="iniPHP.css" />
+    </head>
+    <body>
+        <header>
+            <h1>Premier exercice PHP</h1>
+            <h2>Réalisé par <span class="nom">Geoffrey HERMAN</span></h2>
+        </header>
+        <section>
+            <h2>Question 1</h2>
+              <?php echo "Nous sommes le ".date("d/m/Y").".";?>
+        </section>
+        <section>
+            <h2>Question 2</h2>
+              <?php echo "La version de php est : ".PHP_VERSION." et le systeme d'exploitation est : ".PHP_OS."." ;?>
+        </section>
+        <section>
+            <h2>Question 3</h2>
+            <?php $n=5; $texte="le cinq"?>
+            <?php echo "\$n"." vaut : ".$n." et "."\$texte"." vaut : ".$texte."." ;?>
+        </section>
+        <section>
+            <h2>Question 4</h2>
+            <?php for($i = 0;$i < 5;$i++){
+                    echo $texte."<br>";
+            }?>
+        </section>
+        <section>
+            <h2>Question 5</h2>
+            <?php function paragrapheTronque($chaine,$i){
+              $resultat = "";
+              for($j = 0;$j<$i;$j++){
+                $resultat = $resultat.$chaine[$j];
+              }
+              return $resultat;
+            }
+            ?>
+            <?php echo paragrapheTronque($a="bonjour",$b=2)."<br>";?>
+
+            <?php
+            for($k = 0;$k< strlen($texte)+1;$k++){
+              echo substr($texte,0,$k)."<br>";
+            }
+
+            ;?>
+
+
+        </section>
+        <section>
+            <h2>Question 7</h2>
+            <?php
+            echo "<ul>";
+            for($l = 1;$l<10;$l++){
+              echo "<li>"." 2 * ".$l." = ".$l*2;
+            }
+            echo "</ul>";
+            ;?>
+        </section>
+        <section>
+            <h2>Question 4</h2>
+        </section>
+    </body>
+</html>
